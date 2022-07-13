e270ba19eeb3226f26df1b7e029b172bd5f37810
terenyamasha <terenyamasha@gmail.com>
2022-07-13 18:31:51 +0300

    tests

--- src/test/java/utils/RandomString.java
+++ src/test/java/utils/RandomString.java
@@ -3,13 +3,14 @@ package utils;
 import java.util.Random;
 
 public class RandomString {
-    public static String getRandomString(int length){
+    public static String getRandomString(){
         int leftLimit = 97; // letter 'a'
         int rightLimit = 122; // letter 'z'
+        int maxlength = 10;
 
         Random random = new Random();
-        StringBuilder buffer = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
+        StringBuilder buffer = new StringBuilder();
+        for (int i = 0; i < maxlength; i++) {
             int randomLimitedInt = leftLimit + (int)
                     (random.nextFloat() * (rightLimit - leftLimit + 1));
             buffer.append((char) randomLimitedInt);
